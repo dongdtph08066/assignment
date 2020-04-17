f93b614d456abb1a5f5a150151f7012ccab4c159
dongdtph08066 <dongdtph08066@gmail.com>
2020-04-17 16:33:37 +0700

    50 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BannerComponent } from './banner/banner.component';
+import { Banner2Component } from './banner2/banner2.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductSeComponent } from './product-se/product-se.component';
+import { HomeComponent } from './home/home.component';
+import { FoterComponent } from './foter/foter.component';
+import { ProductService } from '.service/product.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule , HttpClientModule  ],
+  declarations: [ AppComponent, HelloComponent, BannerComponent, Banner2Component, ProductListComponent, ProductSeComponent, HomeComponent, FoterComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ProductService],
+
 })
 export class AppModule { }
---
+++ src/app/product-se/product-se.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-se',
+  templateUrl: './product-se.component.html',
+  styleUrls: ['./product-se.component.css']
+})
+export class ProductSeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
