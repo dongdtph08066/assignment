3432f34f89e6d12feee97a0057d2180913292941
dongdtph08066 <dongdtph08066@gmail.com>
2020-04-17 19:18:21 +0700

    5 binaries uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ManagerComponent } from './manager/manager.component';
 
 
 
@@ -13,7 +14,7 @@ const routes: Routes =[
   { path: "", redirectTo: 'home', pathMatch: "full"},
   {path: 'home', component: HomeComponent},
    { path: 'product/detail/:id', component: ProductDetailComponent},
-
+ { path: 'manager', component: ManagerComponent},
  
 ];
 @NgModule({
