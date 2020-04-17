0ae3851e38ccaba0aaba7ace7b2d14ae9fed3b7c
dongdtph08066 <dongdtph08066@gmail.com>
2020-04-17 20:54:15 +0700

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ManagerComponent } from './manager/manager.component';
+import { DarbroadComponent } from './darbroad/darbroad.component';
 
 
 
@@ -13,9 +14,10 @@ import { ManagerComponent } from './manager/manager.component';
 const routes: Routes =[
   { path: "", redirectTo: 'home', pathMatch: "full"},
   {path: 'home', component: HomeComponent},
-   { path: 'product/detail/:id', component: ProductDetailComponent},
+  { path: 'product/detail/:id', component: ProductDetailComponent},
  { path: 'manager', component: ManagerComponent},
  {path:"admin", redirectTo: "dashboard", pathMatch: "full"},
+ { path: 'dashboard', component: DarbroadComponent},
  
 ];
 @NgModule({
--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { data } from "../MockData";
+import { Product } from "../Product";
+import { ProductService } from '../product.service';
 @Component({
-  selector: 'app-manager',
-  templateUrl: './manager.component.html',
-  styleUrls: ['./manager.component.css']
+  selector: "app-product-manager",
+  templateUrl: "./product-manager.component.html",
+  styleUrls: ["./product-manager.component.css"]
 })
-export class ManagerComponent implements OnInit {
-
-  constructor() { }
+export class ProductManagerComponent implements OnInit {
+  constructor(
+    private productService: ProductService
+  ) {}
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  // products = data;
+  // selected: Product;
+  showDetail(product) {
+    this.selected = product;
+  }
+  removeItem(id) {
+    return (this.products = this.products.filter(product => product.id != id));
+  }
+    getProducts() {
+   
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    })
+  }
 }
